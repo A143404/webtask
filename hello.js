3fc324c6b3cc57d743c5a5d8d6b848c11f518917
Pankaj Bhagchandani <A143404@agl.com.au>
2018-08-28 14:15:49 +1000

    added master

--- hello.js
+++ hello.js
@@ -4,5 +4,6 @@ import {supportedLodashMethods} from './shared/shared.service';
 module.exports = (ctx, cb) => {
   // Respond with supported lodash methods
    // call an imported function.
+    console.log('This is master');
     cb(null,supportedLodashMethods());
 };
