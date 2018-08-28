8fc1e1f2fb589fe8545339b63543eaed9c32f136
Pankaj Bhagchandani <A143404@agl.com.au>
2018-08-28 11:39:51 +1000

    added mithun to console

--- hello.js
+++ hello.js
@@ -4,6 +4,6 @@ import {supportedLodashMethods} from './shared/shared.service';
 module.exports = (ctx, cb) => {
   // Respond with supported lodash methods
    // call an imported function.
-    console.log('Hello Justine');
+    console.log('Hello Mithun');
     cb(null,supportedLodashMethods());
 };
