e4688f5632c599322b44bdbc004ae86534dd131f
Pankaj Bhagchandani <A143404@agl.com.au>
2018-08-28 14:10:42 +1000

    added justine

--- hello.js
+++ hello.js
@@ -4,5 +4,6 @@ import {supportedLodashMethods} from './shared/shared.service';
 module.exports = (ctx, cb) => {
   // Respond with supported lodash methods
    // call an imported function.
+    console.log('hello justine');
     cb(null,supportedLodashMethods());
 };
