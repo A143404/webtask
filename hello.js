e8f80e66575fb33d8b7c4e507fe97983ba86c6dc
Pankaj <39073202+A143404@users.noreply.github.com>
2018-08-28 11:37:14 +1000

    My feature branch (#2)
    * added console with justine

--- hello.js
+++ hello.js
@@ -4,5 +4,6 @@ import {supportedLodashMethods} from './shared/shared.service';
 module.exports = (ctx, cb) => {
   // Respond with supported lodash methods
    // call an imported function.
+    console.log('Hello Justine');
     cb(null,supportedLodashMethods());
 };
