3cde8149e99828cf1140dce5f0e6e8902acbbe87
Pankaj Bhagchandani <A143404@agl.com.au>
2018-08-28 14:11:07 +1000

    added mithun

--- hello.js
+++ hello.js
@@ -4,6 +4,6 @@ import {supportedLodashMethods} from './shared/shared.service';
 module.exports = (ctx, cb) => {
   // Respond with supported lodash methods
    // call an imported function.
-    console.log('hello justine');
+    console.log('hello mithun');
     cb(null,supportedLodashMethods());
 };
