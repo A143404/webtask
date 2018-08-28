5759948464d53788c74c1fff03c5c117016dca9a
Pankaj Bhagchandani <A143404@agl.com.au>
2018-08-28 13:22:27 +1000

    New message for the combined commit

--- hello.js
+++ hello.js
@@ -4,6 +4,6 @@ import {supportedLodashMethods} from './shared/shared.service';
 module.exports = (ctx, cb) => {
   // Respond with supported lodash methods
    // call an imported function.
-    console.log('Hello Pankaj');
+    console.log('Hello Mithun');
     cb(null,supportedLodashMethods());
 };
